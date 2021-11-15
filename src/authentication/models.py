e39c8eec3dfe0f12ddede1d5e5838787ebde2528
MehediHasanNasim <75909031+MehediHasanNasim@users.noreply.github.com>
2021-11-15 23:59:22 +0600

    second commit

--- src/authentication/models.py
+++ src/authentication/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.db.models import Model 
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class products(models.Model):
+    title =        models.CharField(max_length=255)
+    sku =          models.CharField(max_length=255)
+    description =  models.TextField(max_length=50)
+    created_at =   models.DateTimeField(auto_now_add=True)
+    updated_at =   models.DateTimeField(auto_now_add=True)
+
+
+
+
+class products_image(models.Model):
+    product_id =         models.IntegerField()
+    file_path =          models.CharField(max_length=255)
+    thumbnail =          models.BooleanField()
+    created_at =         models.DateTimeField(auto_now_add=True)
+    updated_at =         models.DateTimeField(auto_now_add=True)
+
+
+class products_variants(models.Model):
+    product_id =         models.IntegerField()
+    variant =            models.CharField(max_length=255)
+    variant_id =         models.IntegerField()
+    product_id =         models.IntegerField()
+    created_at =         models.DateTimeField(auto_now_add=True)
+    updated_at =         models.DateTimeField(auto_now_add=True)
+
+
+
+class products_variants_prices(models.Model):
+    product_variant_one =           models.BigIntegerField()
+    product_variant_two =           models.BigIntegerField()
+    product_variant_three =         models.BigIntegerField()
+    price =                         models.IntegerField()
+    stock =                         models.IntegerField()
+    product_id =                    models.IntegerField()
+    created_at =                    models.DateTimeField(auto_now_add=True)
+    updated_at =                    models.DateTimeField(auto_now_add=True)
+
+
+class variants(models.Model):
+    title =        models.CharField(max_length=255)
+    description =  models.TextField(max_length=50)
+    created_at =   models.DateTimeField(auto_now_add=True)
+    updated_at =   models.DateTimeField(auto_now_add=True)
+
+
+
